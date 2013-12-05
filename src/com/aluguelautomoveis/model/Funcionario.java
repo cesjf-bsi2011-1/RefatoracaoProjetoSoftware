ba024fe984ddb3a67e59bf099f1d9476776dd19a
Tássio de Oliveira Silva Auad <tauad@tauad>
2013-12-05 21:54:16 -0200

    Refatoração da estrutura do MVC.

---
+++ src/com/aluguelautomoveis/model/Funcionario.java
@@ -0,0 +1,275 @@
+
+package com.aluguelautomoveis.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author curso
+ */
+public class Funcionario implements Serializable {
+    private String codigo;
+    private String nome;
+    private String cpf;
+    private String rua;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+    private String telefone;
+    private String email;
+    private Date datanascimento;
+    private String celular;
+    private String sexo;
+    private String usuario;
+    private String senha;
+    
+    public Funcionario(){
+        
+    }
+    
+    public Funcionario(String codigo, String nome, String cpf, String rua, String numero, String complemento, String bairro, String cidade, String estado, String cep, String telefone,String email, String habilitacao, String tipo,Date datanascimento,String celular, String sexo, String usuario, String senha){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.rua = rua;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+        this.telefone = telefone;
+        this.email = email;
+        this.datanascimento = datanascimento;
+        this.celular = celular;
+        this.sexo = sexo;
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+    
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+        public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+    
+    public String Exibir(){
+        String str = "";
+        str+= "Código:"+codigo+"\nNome:"+nome+"\nSexo:"+sexo+"\nData Nascimento:"+datanascimento+"\nCPF:"+cpf+"\nRua:"+rua+"\nNúmero:"+numero+
+       "\nComplemento:"+complemento+"\nBairro:"+bairro+"\nCidade:"+cidade+"\nEstado:"+estado+"\nCep:"+cep+"\nTelefone:"+telefone+"\nCelular:"
+       +celular+"\nEmail:"+email+"\nUsuario:"+usuario+"\nSenha:"+senha+"\n";
+        return str;
+    }
+    
+    boolean validarCliente(String c){
+        if(c.equals(cpf)){
+            return true;
+        }
+        else
+            return false;
+    }
+
+   
+     /**
+     * @return the rua
+     */
+    public String getRua() {
+        return rua;
+    }
+
+    /**
+     * @param rua the rua to set
+     */
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    /**
+     * @return the numero
+     */
+    public String getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the complemento
+     */
+    public String getComplemento() {
+        return complemento;
+    }
+
+    /**
+     * @param complemento the complemento to set
+     */
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    /**
+     * @return the bairro
+     */
+    public String getBairro() {
+        return bairro;
+    }
+
+    /**
+     * @param bairro the bairro to set
+     */
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    /**
+     * @return the cidade
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the cep
+     */
+    public String getCep() {
+        return cep;
+    }
+
+    /**
+     * @param cep the cep to set
+     */
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    /**
+     * @return the datanascimento
+     */
+    public Date getDatanascimento() {
+        return datanascimento;
+    }
+
+    /**
+     * @param datanascimento the datanascimento to set
+     */
+    public void setDatanascimento(Date datanascimento) {
+        this.datanascimento = datanascimento;
+    }
+
+    /**
+     * @return the celular
+     */
+    public String getCelular() {
+        return celular;
+    }
+
+    /**
+     * @param celular the celular to set
+     */
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    public Object[] getFuncionario()
+    {
+        Object c[] = {codigo,nome,cpf,telefone};
+        return c;
+    }
+    
+}
---
+++ src/com/aluguelautomoveis/model/dao/ClienteDao.java
@@ -0,0 +1,151 @@
+package com.aluguelautomoveis.model.dao;
+
+import com.aluguelautomoveis.model.Cliente;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class ClienteDao {
+    
+    private static ArrayList<Cliente> lista = new ArrayList<Cliente>();
+    
+    public static void addCliente(Cliente cliente){
+           abrir();
+           lista.add(cliente);
+           salvar();
+}
+    
+    public static Cliente getCliente (String cpf){
+        abrir();
+    int pos = -1;
+    
+    for (int i = 0; i < lista.size(); i++){
+        
+        if (lista.get(i).getCpf().equals(cpf))
+            pos = i;
+            }
+    if (pos == -1)
+         
+        return null;
+                        else 
+                            return lista.get(pos);               
+    }
+      
+     public static boolean removerCliente(String cpf){
+         abrir();
+         
+        int posicao = localizar(cpf);
+        if (posicao != -1)
+        {
+            lista.remove(posicao);
+            salvar();
+            return true;
+        }
+        else
+            return false;
+    
+       }
+     
+     public static void atualizarCliente (Cliente cliente){
+            abrir();
+            
+            int posicao = localizar(cliente.getCpf());
+        
+            lista.remove(posicao);
+        
+            lista.add(posicao,cliente);
+        salvar();
+         }
+        
+     
+    
+          private static int localizar(String cpf)
+    {
+        int pos = -1;
+        for(int i =0;i<lista.size();i++)
+        {
+            if(lista.get(i).getCpf().equals(cpf))
+                pos = i;
+        }
+        
+        return pos;
+    }
+     
+     public static ArrayList<Cliente> getTodosClientes()
+    { abrir();
+    
+        return lista;
+    }
+         
+    private static void abrir(){
+                  
+             File f = new File("Clientes.dat");
+                           
+             if (!f.exists()){
+                       
+                 try {
+                   f.createNewFile();
+                   salvar();
+                 } catch (IOException ex) {
+                     Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+                 }
+                 
+             }
+       
+            ObjectInputStream in = null;
+         
+                              
+        try {
+            in = new ObjectInputStream(new FileInputStream("Clientes.dat"));
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            
+            lista = (ArrayList<Cliente>)in.readObject();
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            in.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+             
+        }
+             
+         private static void salvar(){
+             ObjectOutputStream out = null;
+        try {
+            out = new ObjectOutputStream(new FileOutputStream("Clientes.dat"));
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {                                       
+            out.writeObject(lista);
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {        
+            out.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      
+             
+         }
+         
+         
+         
+}
